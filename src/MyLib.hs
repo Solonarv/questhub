842850d06f76c3ff6226788a56839d6f97b76093
Solonarv <nstamm@gmx.de>
2024-03-21 21:07:25 +0100

    is not helpful

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,21 +1,26 @@
 module MyLib (main) where
 
 import Control.Monad (msum)
+import Control.Monad.IO.Class (liftIO)
 import Data.Functor (void)
 
 import Happstack.Server (nullConf, simpleHTTP, toResponse, ok, ServerPartT
-  , dir, nullDir)
+  , dir, nullDir, Response)
 import HSP.Monad                  (HSPT(..))
+import HSP.XML                    (XML)
 import HSP.XMLGenerator           (unXMLGenT)
 import Happstack.Server.HSP.HTML
 
 import qualified Questhub.Index
 
 main :: IO ()
-main = simpleHTTP nullConf handlers
+main = do
+  putStrLn "starting server..."
+  simpleHTTP nullConf handlers
 
-handlers :: ServerPartT IO ()
+handlers :: ServerPartT IO Response
 handlers = msum
-  [ void $ nullDir >> unHSPT (unXMLGenT Questhub.Index.index)
-  , void $ dir "hello" $ ok "Hello, World!"
+  [ nullDir >> do
+      liftIO $ putStrLn "GET /"
+      toResponse <$> unHSPT (unXMLGenT Questhub.Index.index)
   ]
